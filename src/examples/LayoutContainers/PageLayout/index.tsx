92b909c395aa0a37fa2feb29f355ec357952f686
associaasppibra <associaasppibra@gmail.com>
2025-11-20 13:39:43 +0000

    fix: Corrige erro de linting que impedia o build

--- src/examples/LayoutContainers/PageLayout/index.tsx
+++ src/examples/LayoutContainers/PageLayout/index.tsx
@@ -39,12 +39,7 @@ function PageLayout({ background, children }: Props): JSX.Element {
   }, [pathname]);
 
   return (
-    <MDBox
-      width="100%"
-      height="100%"
-      minHeight="100vh"
-      bgColor={background}
-    >
+    <MDBox width="100%" height="100%" minHeight="100vh" bgColor={background}>
       {children}
     </MDBox>
   );
